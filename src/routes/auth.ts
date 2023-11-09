9036ab9f749a5754c930a1fb80154c04bbf537ae
Toluwanimi <85632246+nimi-io@users.noreply.github.com>
2023-11-10 00:08:37 +0100

    .env.example and tsconfig

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,8 +5,11 @@ const authRouter = Router();
 
 authRouter.post("/login", loginController);
 authRouter.post("/register", registerController);
+
+authRouter.post("/logout", notImplementedController);
 authRouter.post("/generatToken", notImplementedController);
 authRouter.post("/verifyToken", notImplementedController);
 authRouter.post("/resetPassword", notImplementedController);
+
 
 export default authRouter;
